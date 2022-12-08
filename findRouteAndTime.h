c2ce4258a9c04d9bda116f68a0ff08ab86809a76
jacobhboy <jacobhboy66@gmail.com>
2022-12-08 15:28:56 +0900

    add : 다익스트라 알고리즘에 캐싱기능 추가

--- findRouteAndTime.h
+++ findRouteAndTime.h
@@ -9,25 +9,29 @@
 
 #endif //YOONCHANG_FINDROUTEANDTIME_H
 
-int shortestRoute[26];
-bool isChecked[26];
+int distence[26]; // 다익스트라 알고리즘에서 경로별 최단거리 확인 알고리즘
+bool isChecked[26]; // 다익스트라 알고리즘에서 노드가 체크되었는지 확인하는 배열
+
+bool isUsedBefore[26]; // 이 노드에서 예전에 출발 혹은 도착되는데 사용되었는지확인
 
 
 int findShortestRoute(){
     int min = INF;
     int index;
     for(int i = 0 ; i < NODE_COUNT ; i++){
-        if(shortestRoute[i] < min && !isChecked[i]){
-            min = shortestRoute[i];
+        if(distence[i] < min && !isChecked[i]){
+            min = distence[i];
             index = i;
         }
     }
     return index;
 }
 
-int dijkstra(int start){
+int dijkstraLogic(int start){
+    isUsedBefore[start] = true; //전에 사용이 되었다!
+
     for(int i = 0 ; i < NODE_COUNT ; i++){
-        shortestRoute[i] = shortestRouteTable[start][i];
+        distence[i] = shortestRouteTable[start][i];
     }
     isChecked[start] = true;
     for(int i = 0 ; i < NODE_COUNT -2 ; i++){
@@ -35,10 +39,23 @@ int dijkstra(int start){
         isChecked[current] = true;
         for(int j = 0 ; j < NODE_COUNT - 2; j++){
             if(!isChecked[j]){
-                if(shortestRoute[j] + shortestRouteTable[start][j] < shortestRoute[j]){
-                    shortestRoute[j] = shortestRoute[j] + shortestRouteTable[start][j];
+                if(distence[j] + shortestRouteTable[start][j] < distence[j]){
+                    distence[j] = distence[j] + shortestRouteTable[start][j];
                 }
             }
         }
     }
+    //찾았던거 다시 최단경로로 넣어서 캐싱
+    for(int i = 0 ; i < NODE_COUNT ; i++){
+        shortestRouteTable[start][i] = distence[i];
+    }
+}
+
+
+int dijnstraService(int start, int end){
+    if(isUsedBefore[start]) return shortestRouteTable[start][end];
+    if(isUsedBefore[end]) return shortestRouteTable[start][end];
+    else dijkstraLogic(start);
+
+    return shortestRouteTable[start][end];
 }
--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include "shortestRouteTable.h"
+#include "findRouteAndTime.h"
 
 int main() {
     stationInit();
     tableInit();
 
-    for(int i = 0 ; i < NODE_COUNT ; i++, puts(""))
+    for(int i = 0 ; i < NODE_COUNT ; i++)
         for(int j = 0 ; j < NODE_COUNT ; j++)
             printf("%d ",shortestRouteTable[i][j]);
 
