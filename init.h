1ffd098e431969e189128605f472cca0ee458061
jacobhboy <jacobhboy66@gmail.com>
2022-12-08 22:23:10 +0900

    update : init이라는 header파일을 만들어서 초기화를 한번에 할수있게 변경

---
+++ init.h
@@ -0,0 +1,18 @@
+//
+// Created by 이창보 on 2022/12/08.
+//
+
+#include "findRouteAndTime.h"
+
+#ifndef YOONCHANG_INIT_H
+#define YOONCHANG_INIT_H
+
+#endif //YOONCHANG_INIT_H
+
+char start[100];
+char end[100];
+
+void init(){
+    stationInit();
+    tableInit();
+}
--- main.c
+++ main.c
@@ -1,12 +1,10 @@
-#include "findRouteAndTime.h"
+#include "init.h"
 
-int main() {
+ int main() {
 
-    char start[100];
-    char end[100];
+    init();
 
-    stationInit();
-    tableInit();
+    printf("--------1호선--------\nBusanDae\nDongLae\nYounSan\nYaangJeong\nSeoMyeon\nJwaCheon\nBusan\nSuYeog\n--------2호선--------\nSeogDae\nSeoDong\nNagmin\nDongLae\nMiNam\n--------2호선--------\nDeogCheon\nNamsanJeong\nMiNam\nYounSan\nBaeSan\nSuYeong\n--------2호선--------\nYulRi\nDeogCheon\nMoRa\nSaSang\nNaengJeong\nGaYa\nSeoMyeon\nMunHyeon\nDaeYeon\nGeumLyeonSan\nSuYeog");
 
 //    for(int i = 0 ; i < NODE_COUNT ; i++, puts(""))
 //        for(int j = 0 ; j < NODE_COUNT ; j++)
