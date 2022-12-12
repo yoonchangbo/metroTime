968dc1f3a8172d958d5eaa50f2cdf06331740e42
jacobhboy <jacobhboy66@gmail.com>
2022-12-12 21:28:24 +0900

    finished

--- main.c
+++ main.c
@@ -5,9 +5,5 @@
     init();
     logic();
 
-    for(int i=0;i<NODE_COUNT;i++, puts(""))
-        for(int j = 0 ; j < NODE_COUNT ;j++)
-            printf("%s ", routeTable[i][j]);
-
     return 0;
 }
